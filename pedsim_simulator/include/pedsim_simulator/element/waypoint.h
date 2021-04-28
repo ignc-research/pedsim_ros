9f8a846db8673b0e028fde21408ad8feabd076ca
dangaw255 <danielgawrischgithub@gmail.com>
2021-04-28 17:20:25 +0200

    init staticObstacleAngle

--- pedsim_simulator/include/pedsim_simulator/element/waypoint.h
+++ pedsim_simulator/include/pedsim_simulator/element/waypoint.h
@@ -58,7 +58,7 @@ class Waypoint : public ScenarioElement, public Ped::Twaypoint {
   virtual void sety(double yIn);
   virtual void setRadius(double rIn);
 
-  double staticObstacleAngle;
+  double staticObstacleAngle = 0.0;
 
   // Attributes
  protected:
