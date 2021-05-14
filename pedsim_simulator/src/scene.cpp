3cc1d86d468b0d3850b3f65b3c57b839de636fe6
dangaw255 <danielgawrischgithub@gmail.com>
2021-05-14 22:23:28 +0200

    fix bug object currentGroup gets deleted but the current iteration of the for loop is not cancelled. added continue statement to prevent access of free'd memory

--- pedsim_simulator/src/scene.cpp
+++ pedsim_simulator/src/scene.cpp
@@ -267,6 +267,7 @@ void Scene::dissolveClusters() {
       if (currentGroup->memberCount() == 1) {
         // we don't need one agent groups
         delete currentGroup;
+        continue;
       } else if (currentGroup->memberCount() > 1) {
         // keep track of groups
         agentGroups.append(currentGroup);
