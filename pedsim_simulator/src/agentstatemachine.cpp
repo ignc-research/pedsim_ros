eb276b48362882248e3ee2e377a1966498f227dc
basselfa <38949665+basselfa@users.noreply.github.com>
2021-06-10 17:02:11 +0200

    Update agentstatemachine.cpp

--- pedsim_simulator/src/agentstatemachine.cpp
+++ pedsim_simulator/src/agentstatemachine.cpp
@@ -726,7 +726,7 @@ void AgentStateMachine::activateState(AgentState stateIn) {
       agent->enableForce("KeepDistance");
       agent->keepDistanceTo = SCENE.robot->getPosition();
       agent->keepDistanceForceDistance = 3.1;
-      agent->setVmax(agent->vmaxDefault * 1.8);
+      agent->setVmax(agent->vmaxDefault * 2.0);
       break;
   }
 
