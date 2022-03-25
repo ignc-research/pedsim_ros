d6c22bcad8d7ee0963c19b43e209787da30e2fdc
eliastreis <elias.treis@gmail.com>
2022-03-26 00:16:58 +0100

    clean branch

--- pedsim_simulator/src/agent_pose_stamped.cpp
+++
@@ -1,8 +0,0 @@
-#include <pedsim_simulator/agent_pose_stamped.h>
-
-AgentPoseStamped::AgentPoseStamped(ros::Time timestamp_in, Ped::Tvector pos_in, double theta_in) {
-  timestamp = timestamp_in;
-  pos = pos_in;
-  theta = theta_in;
-}
-
